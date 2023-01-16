bafaa20ec6a25cbf559af056d0d8be41f64e97c8
Artemet <60576459+Artemet@users.noreply.github.com>
2023-01-16 17:56:37 +0300

    Update index.php

--- index.php
+++ index.php
@@ -1,6 +1,16 @@
 <?php
-    include "layouts/sidebar.php";
+    include "layouts/header.php";
 ?>
-<!-- code -->
+<div class="page_colume">
+    <?php
+        include "layouts/sidebar.php";
+    ?>
+    <div class="web_part">
+        <div class="page_name">
+            <h1>Hi<span>,</span> <br> I’m Artem<span>,</span>
+             <br> web developer<span>.</span></h1>
+        </div>
+    </div>
+</div>
 </body>
 </html>
